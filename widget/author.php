d7b2c4f6693ad30c374cabc1b252ea96b4bffe66
Taufik Nurrohman <hi@taufik-nurrohman.com>
2024-05-17 20:10:53 +0700

    Remove Avatar

--- widget/author.php
+++ widget/author.php
@@ -4,16 +4,16 @@ $author = $page->author ?? 0;
 $content = "";
 if ($author) {
     if ($author instanceof User) {
-        if ($avatar = $author->avatar(72)) {
-            $content .= '<figure>';
-            $content .= '<img alt="" height="72" src="' . $avatar . '" width="72">';
-            $content .= '<figcaption>';
+        // if ($avatar = $author->avatar(72)) {
+        //     $content .= '<figure>';
+        //     $content .= '<img alt="" height="72" src="' . $avatar . '" width="72">';
+        //     $content .= '<figcaption>';
+        //     $content .= '<p><a href="' . ($author->link ?? $author->url) . '" rel="author">' . $author . '</a></p>';
+        //     $content .= '</figcaption>';
+        //     $content .= '</figure>';
+        // } else {
             $content .= '<p><a href="' . ($author->link ?? $author->url) . '" rel="author">' . $author . '</a></p>';
-            $content .= '</figcaption>';
-            $content .= '</figure>';
-        } else {
-            $content .= '<p><a href="' . ($author->link ?? $author->url) . '" rel="author">' . $author . '</a></p>';
-        }
+        // }
         $content .= '<p>' . $author->description . '</p>';
         $content .= '<p><time datetime="' . $author->time->format('c') . '" role="status">' . i('Joined since %s', $author->time('%B %Y')) . '</time></p>';
     } else {
